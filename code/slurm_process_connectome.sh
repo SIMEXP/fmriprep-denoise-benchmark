ba3095f1e3bd1b260705c7be7351600bb46f7d68
Hao-Ting Wang <htwangtw@gmail.com>
2021-12-03 15:57:04 -0500

    add slurm script

--- code/slurm_process_connectome.sh
+++ code/slurm_process_connectome.sh
@@ -3,13 +3,10 @@
 #SBATCH --time=12:00:00
 #SBATCH --mem=4G 
 #SBATCH --account=rrg-pbellec
-#SBATCH --output={jobname}.out
-#SBATCH --error={jobname}.err
+#SBATCH --output=connectome.out
+#SBATCH --error=connectome.err
 
 
 OUTPUT="/${HOME}/scratch/test"
-
 workon fmriprep-denoise-benchmark
-
 python code/process_connectome.py ${OUTPUT} --atlas difumo 
-
