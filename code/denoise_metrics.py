f77a7018a74896cbef2c374157f2abc2c5fcb4b7
Hao-Ting Wang <htwangtw@gmail.com>
2021-12-06 13:54:28 -0500

    Adapt for tar archieve

--- code/denoise_metrics.py
+++ code/denoise_metrics.py
@@ -9,6 +9,8 @@ To-do:
 - consider other metrics - perhaps look at Parker 2018
 
 """
+import tarfile
+import io
 from os import sep
 from pathlib import Path
 import json
@@ -20,28 +22,31 @@ from metrics import quality_control_connectivity
 # define path of input and output
 STRATEGY = "code/benchmark_strategies.json"
 OUTPUT = "inputs/interim"
-INPUT_CONNECTOMES = "inputs/"
+INPUT = "inputs/dataset-ds000288.tar.gz"
 
 
 def main():
     """Main function."""
     strategy_file = Path(__file__).parents[1] / STRATEGY
     output = Path(__file__).parents[1] / OUTPUT
-    input_connectomes = Path(__file__).parents[1] / INPUT_CONNECTOMES
+    input_connectomes = Path(__file__).parents[1] / INPUT
     # read the strategy deining files
     with open(strategy_file, "r") as file:
         benchmark_strategies = json.load(file)
 
-    movement = pd.read_csv(input_connectomes / "dataset-ds000288_desc-movement_phenotype.tsv",
-                           sep='\t', index_col=0, header=0)
+    with tarfile.open(input_connectomes, 'r:gz') as tar:
+        movement = tar.extractfile('dataset-ds000288/dataset-ds000288_desc-movement_phenotype.tsv').read()
+        movement = pd.read_csv(io.BytesIO(movement),
+                               sep='\t', index_col=0, header=0, encoding='utf8')
+
 
     metric_per_edge, sig_per_edge = pd.DataFrame(), pd.DataFrame()
 
     for strategy_name in benchmark_strategies:
         print(strategy_name)
-
-        dataset_connectomes = pd.read_csv(input_connectomes / f"dataset-ds000288_atlas-schaefer7networks_nroi-100_desc-{strategy_name}_data.tsv",
-                                          sep='\t', index_col=0, header=0)
+        with tarfile.open(input_connectomes, 'r:gz') as tar:
+            connectome = tar.extractfile(f'dataset-ds000288/atlas-schaefer/dataset-ds000288_atlas-schaefer7networks_nroi-400_desc-{strategy_name}_data.tsv').read()
+            dataset_connectomes = pd.read_csv(io.BytesIO(connectome), sep='\t', index_col=0, header=0)
         # QC-FC per edge
         cur_qc_fc, cur_sig = quality_control_connectivity(movement, dataset_connectomes)
         # dump the results
