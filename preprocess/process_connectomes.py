ee406436fc75b0f28b8a8c282edfadd0f1ad371d
Hao-Ting Wang <htwangtw@gmail.com>
2021-12-13 15:01:57 -0500

    sort output by subject id

--- preprocess/process_connectomes.py
+++ preprocess/process_connectomes.py
@@ -88,6 +88,7 @@ def main():
             func_data = data_aroma.func if "aroma" in name else data.func
             valid_subject_ts, valid_subject_id = _dataset_timeseries(nroi, output, atlas, name, parameters, strategy, func_data)
             dataset_connectomes = _compute_connectome(valid_subject_ts, valid_subject_id)
+            dataset_connectomes = dataset_connectomes.sort_index()
             output_connectome = output / f"dataset-ds000288_atlas-{atlas_name}_nroi-{nroi}_desc-{name}_data.tsv"
             dataset_connectomes.to_csv(output_connectome, sep='\t')
 
@@ -107,6 +108,7 @@ def _dataset_timeseries(nroi, output, atlas, name, parameters, strategy, func_da
 def _calculate_movement_stats(output, data):
     if not Path(output / "dataset-ds000288_desc-movement_phenotype.tsv").is_file():
         movement = ds000288_movement(data)
+        movement = movement.sort_index()
         movement.to_csv( output / "dataset-ds000288_desc-movement_phenotype.tsv", sep='\t')
         print("Generate movement stats.")
 
