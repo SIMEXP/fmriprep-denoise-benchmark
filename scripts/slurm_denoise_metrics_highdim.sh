27af82975cb7b7a07a3d58c1f5542ca6a8f06dfc
Hao-Ting Wang <htwangtw@gmail.com>
2022-05-26 15:54:30 -0400

    FIX typo in file name....

--- scripts/slurm_denoise_metrics_highdim.sh
+++ scripts/slurm_denoise_metrics_highdim.sh
@@ -14,25 +14,25 @@ source /home/${USER}/.virtualenvs/fmriprep-denoise-benchmark/bin/activate
 cd /home/${USER}/projects/def-pbellec/${USER}/fmriprep-denoise-benchmark/
 
 python ./fmriprep_denoise/features/build_features.py \
-    "/home/${USER}/projects/def-pbellec/${USER}/fmriprep-denoise-benchmark/inputs/dataset-ds000228.tar.gz" \
-    ${OUTPUT} \
-    --atlas difumo \
-    --dimension 1024
-
-python ./fmriprep_denoise/features/build_features.py \
     "/home/${USER}/projects/def-pbellec/${USER}/fmriprep-denoise-benchmark/inputs/dataset-ds000030.tar.gz" \
-    ${OUTPUT} \
-    --atlas difumo \
-    --dimension 1024
-
-python ./fmriprep_denoise/features/build_features.py \
-    "/home/${USER}/projects/def-pbellec/${USER}/fmriprep-denoise-benchmark/inputs/dataset-ds000228.tar.gz" \
     ${OUTPUT} \
     --atlas schaefer7networks \
     --dimension 1000
 
 python ./fmriprep_denoise/features/build_features.py \
     "/home/${USER}/projects/def-pbellec/${USER}/fmriprep-denoise-benchmark/inputs/dataset-ds000030.tar.gz" \
+    ${OUTPUT} \
+    --atlas difumo \
+    --dimension 1024
+
+python ./fmriprep_denoise/features/build_features.py \
+    "/home/${USER}/projects/def-pbellec/${USER}/fmriprep-denoise-benchmark/inputs/dataset-ds000228.tar.gz" \
+    ${OUTPUT} \
+    --atlas difumo \
+    --dimension 1024
+
+python ./fmriprep_denoise/features/build_features.py \
+    "/home/${USER}/projects/def-pbellec/${USER}/fmriprep-denoise-benchmark/inputs/dataset-ds000228.tar.gz" \
     ${OUTPUT} \
     --atlas schaefer7networks \
     --dimension 1000
--- scripts/slurm_denoise_metrics_mist.sh
+++ scripts/slurm_denoise_metrics_mist.sh
@@ -17,14 +17,13 @@ source /home/${USER}/.virtualenvs/fmriprep-denoise-benchmark/bin/activate
 cd /home/${USER}/projects/def-pbellec/${USER}/fmriprep-denoise-benchmark/
 
 python ./fmriprep_denoise/features/build_features.py \
-    "/home/${USER}/projects/def-pbellec/${USER}/fmriprep-denoise-benchmark/inputs/dataset-ds000228.tar.gz" \
-    ${OUTPUT} \
-    --atlas mist \
-    --dimension ${DIMENSIONS[${SLURM_ARRAY_TASK_ID}]}
-
-python ./fmriprep_denoise/features/build_features.py \
     "/home/${USER}/projects/def-pbellec/${USER}/fmriprep-denoise-benchmark/inputs/dataset-ds000030.tar.gz" \
     ${OUTPUT} \
     --atlas mist \
     --dimension ${DIMENSIONS[${SLURM_ARRAY_TASK_ID}]}
     
+python ./fmriprep_denoise/features/build_features.py \
+    "/home/${USER}/projects/def-pbellec/${USER}/fmriprep-denoise-benchmark/inputs/dataset-ds000228.tar.gz" \
+    ${OUTPUT} \
+    --atlas mist \
+    --dimension ${DIMENSIONS[${SLURM_ARRAY_TASK_ID}]}
