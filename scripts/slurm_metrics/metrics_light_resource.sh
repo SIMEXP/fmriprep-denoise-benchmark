532e703f93745402ed67d8dbbf95586d0953d1e4
Hao-Ting Wang <htwangtw@gmail.com>
2022-11-08 09:31:40 -0500

    improve metric jobs submission

--- scripts/slurm_metrics/metrics_light_resource.sh
+++ scripts/slurm_metrics/metrics_light_resource.sh
@@ -1,51 +1,48 @@
 #!/bin/bash
-#SBATCH --job-name=qcfc_gordon
+#SBATCH --job-name=metrics_lowdim
 #SBATCH --time=24:00:00
 #SBATCH --account=rrg-pbellec
-#SBATCH --output=logs/qcfc_gordon.out
-#SBATCH --error=logs/qcfc_gordon.err
+#SBATCH --output=logs/metrics_lowdim.%a.out
+#SBATCH --error=logs/metrics_lowdim.%a.err
 #SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=4G 
+#SBATCH --mem-per-cpu=2G 
+#SBATCH --array=0-3
 
 
 source /lustre03/project/6003287/${USER}/.virtualenvs/fmriprep-denoise-benchmark/bin/activate
 cd /home/${USER}/projects/rrg-pbellec/${USER}/fmriprep-denoise-benchmark/
 OUTPUT=inputs/denoise-metrics/
 
-echo "gordon333"
-build_features inputs/giga_timeseries/ds000030/fmriprep-20.2.1lts/ \
-    ${OUTPUT} \
-    --atlas gordon333 --dimension 333 --qc stringent
-build_features inputs/giga_timeseries/ds000228/fmriprep-20.2.1lts/ \
-    ${OUTPUT} \
-    --atlas gordon333 --dimension 333 --qc stringent
+DATASETS=(ds000030 ds000228 ds000030 ds000228)
+METRICS=(modularity qcfc qcfc modularity)
+ds=${DATASETS[${SLURM_ARRAY_TASK_ID}]}
+met=${METRICS[${SLURM_ARRAY_TASK_ID}]}
 
-echo "mist"
-for N in 7 12 20 36 64 122 197 325 444 ROI; do 
-    build_features inputs/giga_timeseries/ds000228/fmriprep-20.2.1lts/ \
-        ${OUTPUT} \
-        --atlas mist --dimension $N --qc stringent
-    build_features inputs/giga_timeseries/ds000030/fmriprep-20.2.1lts/ \
-        ${OUTPUT} \
-        --atlas mist --dimension $N --qc stringent
-done 
+echo $ds
+echo $met
 
 echo "schaefer7networks"
-for N in 100 200 300 400 500 600; do 
-    build_features inputs/giga_timeseries/ds000228/fmriprep-20.2.1lts/ \
+for N in 600 100 200 300 400 500; do 
+    build_features inputs/giga_timeseries/${ds}/fmriprep-20.2.5lts/ \
         ${OUTPUT} \
-        --atlas schaefer7networks --dimension $N --qc stringent
-    build_features inputs/giga_timeseries/ds000030/fmriprep-20.2.1lts/ \
-        ${OUTPUT} \
-        --atlas schaefer7networks --dimension $N --qc stringent
+        --atlas schaefer7networks --dimension $N --qc stringent --metric ${met}
 done 
 
 echo "difumo"
-for N in 64 128 256 512; do 
-    build_features inputs/giga_timeseries/ds000228/fmriprep-20.2.1lts/ \
+for N in 512 64 128 256 512; do 
+    build_features inputs/giga_timeseries/${ds}/fmriprep-20.2.5lts/ \
         ${OUTPUT} \
-        --atlas difumo --dimension $N --qc stringent
-    build_features inputs/giga_timeseries/ds000030/fmriprep-20.2.1lts/ \
+        --atlas difumo --dimension $N --qc stringent --metric ${met}
+done 
+
+echo "gordon333"
+build_features inputs/giga_timeseries/${ds}/fmriprep-20.2.5lts/ \
+    ${OUTPUT} \
+    --atlas gordon333 --dimension 333 --qc stringent --metric ${met}
+echo "mist"
+
+for N in 325 444 ROI 7 12 20 36 64 122 197; do 
+    build_features inputs/giga_timeseries/${ds}/fmriprep-20.2.5lts/ \
         ${OUTPUT} \
-        --atlas difumo --dimension $N --qc stringent
+        --atlas mist --dimension $N --qc stringent --metric ${met}
 done 
